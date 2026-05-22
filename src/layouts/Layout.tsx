514a62311ff86b0a2b26f4e835a036dea2ef1a50
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-22 16:42:19 +0300

    added scroll to top feature

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    }, [location.pathname]);
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <Outlet />
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
