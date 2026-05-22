b800ce6a564fc63c09c39dbb375ee9f73d667271
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-22 18:31:38 +0300

    added back to top feat and enhanced scrolling

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
+import AOS from 'aos';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import BackToTop from '../components/BackToTop';
 
 export default function Layout() {
     const location = useLocation();
 
     useEffect(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+        AOS.refresh();
     }, [location.pathname]);
 
     return (
@@ -15,6 +18,7 @@ export default function Layout() {
             <Navbar />
             <Outlet />
             <Footer />
+            <BackToTop />
         </div>
     );
 }
